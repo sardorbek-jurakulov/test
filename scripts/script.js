8ea17966e02dd83b4a655c55d06d990f1b523148
sardorbek-jurakulov <sardorbek.juraqulov@gmail.com>
2021-11-22 11:58:19 +0500

    WIP

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,18 @@
 let data = {
-  title: "App title",
-
+  title: "App title"
 }
 
 let vm1 = new Vue({
-  
+  data: data  
 });
 
+vm1.$mount("#app1");
+
+
 let vm2 = new Vue({});
+
+let vm3 = new Vue({
+  template: '<h1>Assalamu alaykum!</h1>'
+});
+
+vm3.$mount('#app2');
