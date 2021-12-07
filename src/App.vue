33cfb6ec922784bdf92dc7831f186ca201ba9996
sardorbek-jurakulov <sardorbek.juraqulov@gmail.com>
2021-12-07 07:40:42 +0500

    WIP

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
         <app-header></app-header>
         <hr>
         <div class="row">
-            <servers></servers>
+            <servers :servers="servers" :selectedElementId="selectedElementId"></servers>
             <app-server-details></app-server-details>
         </div>
         <hr>
@@ -12,19 +12,36 @@
 </template>
 
 <script>
-    import Header from './components/Shared/Header.vue';
-    import Footer from './components/Shared/Footer.vue';
-    import Servers from './components/Server/Servers.vue';
-    import ServerDetails from './components/Server/ServerDetails.vue';
+import Header from './components/Shared/Header.vue';
+import Footer from './components/Shared/Footer.vue';
+import Servers from './components/Server/Servers.vue';
+import ServerDetails from './components/Server/ServerDetails.vue';
 
-    export default {
-        components: {
-            appHeader: Header,
-            Servers,
-            'app-server-details': ServerDetails,
-            'app-footer': Footer
-        }
+export default {
+  data() {
+    return {
+      servers: [
+        {id: 1, technology: "Python"},
+        {id: 2, technology: "JavaScript"},
+        {id: 3, technology: "PHP"},
+        {id: 4, technology: "Rust"},
+        {id: 5, technology: "Golang"},
+        {id: 6, technology: "Flutter"},
+        {id: 7, technology: "Java"},
+        {id: 8, technology: "C++"},
+        {id: 9, technology: "C#"},
+        {id: 10, technology: "Ruby"},
+      ],
+      selectedElementId: null
     }
+  },
+  components: {
+      appHeader: Header,
+      Servers,
+      'app-server-details': ServerDetails,
+      'app-footer': Footer
+  }
+}
 </script>
 
 <style>
--- src/components/Server/Servers.vue
+++ src/components/Server/Servers.vue
@@ -1,16 +1,32 @@
 <template>
     <div class="col-xs-12 col-sm-6">
         <ul class="list-group">
-            <li
-                    class="list-group-item"
-                    v-for="index in 5">
-                Server #{{ index }}
+            <li @click="elementSelector" class="list-group-item" v-for="index in servers" :key="index.id">
+                Server #{{ index.id }}
             </li>
         </ul>
     </div>
 </template>
 
 <script>
+export default {
+  props: {
+    servers: {
+      type: Array,
+      required: true
+    },
+    selectedElementId: {
+      type: Number,
+      required: true
+    }
+  },
+  methods: {
+    elementSelector(env) {
+      this.selectedElementId = env.target.value;
+      console.log(this.selectedElementId);
+    }
+  }
+}
 </script>
 
 <style>
